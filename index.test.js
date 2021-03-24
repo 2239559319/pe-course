c7d1049e841524956eba361269fbd794620d3618
2239559319 <w2239559319@outlook.com>
2021-03-24 13:02:07 +0800

    add jest

---
+++ index.test.js
@@ -0,0 +1,36 @@
+const {
+  app_key,
+  app_secret,
+  getSign
+} = require('./lib/index.cjs')
+
+const getData = {
+  app_key: 'eb8c68399de7483abb2d8abaea0d039f',
+  timestamp: '1616561463000',
+  sign: 'e98f4838036419596a411b0e5cb3b92b',
+  path: '/api/terms'
+}
+
+const postData = {
+  path: '/api/login',
+  app_key: 'eb8c68399de7483abb2d8abaea0d039f',
+  timestamp: '1616561934000',
+  sign: 'b18c82c474d04603806963d757249884',
+  data: {
+    username: '2016141451248',
+    password: '111111'
+  }
+}
+
+describe('test sign', () => {
+  it('app_key test', () => {
+    expect(app_key).toBe(getData.app_key)
+  })
+
+  it('get sign test', () => {
+    expect(getSign(getData.path, app_secret, getData.timestamp)).toBe(getData.sign)
+  })
+  it('post sign test', () => {
+    expect(getSign(postData.path, app_secret, postData.timestamp, postData.data)).toBe(postData.sign)
+  })
+})
